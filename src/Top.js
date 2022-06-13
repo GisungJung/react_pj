4ec97fb47ddf01e2a1b304bb4d444da0ad4b67f2
GisungJung <jgs5027@naver.com>
2022-06-13 23:40:41 +0900

    개발환경셋팅

--- src/Top.js
+++ src/Top.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Study</h1>
+    <div className="Top">
+      <h1>나는탑</h1>
       <input type='button' value="버튼"></input>
     </div>
   );
