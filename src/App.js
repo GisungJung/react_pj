2f2bfbcc469a3fb0bf34e6d5acccb3733586f07e
coding-kevin <gisungj5027@gmail.com>
2022-06-15 17:46:33 +0900

    state 익히기
    (cherry picked from commit aa3fe19ee7fecaf904330daeb85ed76df6b164f6)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import { Component } from 'react';
 import './App.css';
-import TOC from './components/TOC'; // TOC 가져옴
-import Contents from './components/Contents'; // TOC 가져옴
-import Subject from './components/Subject'; // TOC 가져옴
-
+import TOC from './components/TOC'; 
+import Content from './components/Content'; 
+import Subject from './components/Subject';
 class App extends Component {
+  //생성자 초기화시킴
+  constructor(props){
+    super(props);
+    this.state = {
+      Subject:{
+                title : "WEB",
+                sub : "World wide Web!"
+              },
+      contents: [
+          {id:1,title:"HTML", desc:"에이치티엠엘 어렵지 않아요!"},
+          {id:2,title:"CSS", desc:"씨에스에스 어렵지 않아요!"},
+          {id:3,title:"JavaScript", desc:"자바스크립트 어렵지 않아요!"}
+      ]
+    }
+  }
+
   render() {
     return(
     <div className="App">
-      <Subject title="WEB" sub="World Wide Web!"></Subject>
-      <TOC></TOC>
-      <Contents title="HTML" desc="에이치티엠엘은 어렵지 않아요"></Contents>
+      <Subject title={this.state.Subject.title} sub={this.state.Subject.sub}></Subject>
+      <TOC data = {this.state.contents}>
+        
+      </TOC>
+      <Content title="HTML" desc="에이치티엠엘은 어렵지 않아요"></Content>
     </div>
     );
   }
