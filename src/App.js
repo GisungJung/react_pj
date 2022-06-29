d4b5ab5ab57d145bf9e96f7d81434fae6ea235ec
coding-kevin <gisungj5027@gmail.com>
2022-06-29 15:14:07 +0900

    2022.06.29 delete 완료

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import TOC from './components/TOC'; 
 import ReadContent from './components/ReadContent'; 
 import CreateContent from './components/CreateContent'; 
+import UpdateContent from './components/UpdateContent'; 
 import Subject from './components/Subject';
 import BtnGrp from './components/BtnGrp';
 
@@ -24,43 +25,62 @@ class App extends Component {
       ]
     }
   }
+  getReadContent(){
+    var i = 0;
+    while(i < this.state.contents.length){
+      var data = this.state.contents[i];
+      if(data.id === this.state.selected_content_id){
+        return data;
+        break;
+      }
+      i=i+1;
+    }
+  }
 
-  render() {
-    console.log('App render');
+  getContent(){
     var _title, _desc, _article = null;
+    
     if (this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     }else if (this.state.mode === 'read'){
-      var i = 0;
-      while(i<this.state.contents.length){
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id){
-          _title = data.title;
-          _desc = data.desc;
-    
-        }
-        i=i+1;
-      }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
     }else if(this.state.mode === 'create'){
       _article= <CreateContent onSubmit={function(_title, _desc){
         //상태에 값 추가
         this.max_content_id = this.max_content_id + 1;
-        // push는 원본 데이터를 바꿈
-        /* this.state.contents.push(
-          {id:this.max_content_id, title: _title, desc: _desc}
-        ); */
-        // concat는 원본 데이터를 바꾸지 않음
-       var _contents =  this.state.contents.concat(
-          {id:this.max_content_id, title: _title, desc: _desc}
-        );
+        var _contents = Array.from(this.state.contents);
+        _contents.push({id:this.max_content_id, title: _title, desc: _desc});
         this.setState({
-          contents:_contents
+          contents:_contents,
+          mode: 'read',
+          selected_content_id : this.max_content_id
         });
       }.bind(this)}></CreateContent> 
+    }else if(this.state.mode === 'update'){
+      _content = this.getReadContent();
+      _article= <UpdateContent data={_content} onSubmit={function(_id, _title, _desc){
+        var _contents = Array.from(this.state.contents);
+        var i = 0;
+        while(i<_contents.length){
+          if(_contents[i].id === _id){
+            _contents[i] = {id:_id, title: _title, desc:_desc}
+            break;
+          }
+          i = i + 1;
+        }
+        this.setState({
+          contents:_contents,
+          mode: 'read'
+        });
+      }.bind(this)}></UpdateContent> 
     }
+    return _article;
+  }
+  render() {
+    console.log('App render');
     return(
     <div className="App">
       {<Subject 
@@ -83,13 +103,32 @@ class App extends Component {
       </TOC>
       <BtnGrp 
           onChangeMode={function(_mode){
-            this.setState({
-              mode : _mode
-            });
+            if(_mode === 'delete'){
+              var _contents = Array.from(this.state.contents);
+               if(window.confirm('really?')){
+                var i = 0;
+                while(i < this.state.contents.length){
+                  if(_contents[i].id === this.state.selected_content_id){
+                    _contents.splice(i,1);
+                    break;
+                  } 
+                  i = i+ 1
+                }
+                this.setState({
+                  mode : 'welcome',
+                  contents : _contents
+                });
+                alert('delete 되었습니다.');
+               } 
+            }else {
+              this.setState({
+                mode : _mode
+              });
+            }
         }.bind(this)}> 
       </BtnGrp>
-      {_article}
-    </div>
+      {this.getContent()}
+      </div>
     );
   }
 }
